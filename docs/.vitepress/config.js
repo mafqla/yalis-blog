1763ce56206fbb28f2509defe6e1b7f017f4a583
mafqla <1692285748@qq.com>
2022-06-26 15:40:33 +0800

    新增vue3文档和图片

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,27 +1,57 @@
 export default {
   // These are app level configs.
-  lang: 'en-US',
+  lang: 'zh-CN',
   title: 'skyseek-notes',
   description: 'Vite & Vue powered static site generator.',
+  lastUpdated: true,
   base: '/',
   themeConfig: {
     siteTitle: '寻觅的前端学习笔记',
     socialLinks: [{ icon: 'github', link: 'https://github.com/mafqla' }],
-    nav: [
-      { text: 'Guide', link: '/guide' },
-      {
-        text: 'Dropdown Menu',
-        items: [
-          { text: 'Item A', link: '/item-1' },
-          { text: 'Item B', link: '/item-2' },
-          { text: 'Item C', link: '/item-3' },
-        ],
-      },
-    ],
-    lastUpdatedText: 'Updated Date',
+    nav: nav(),
+    sidebar: {
+      '/framework/': sidebarFrameword(),
+    },
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2022-present fql',
     },
+    algolia: {
+      appId: '8J64VVRP8K',
+      apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+      indexName: 'skyseek-notes',
+    },
   },
+}
+
+function nav() {
+  return [
+    {
+      text: '框架',
+      items: [
+        {
+          text: 'Vue3',
+          link: '/framework/vue3/basicConcepts',
+          activeMatch: '/framework/vue3/',
+        },
+      ],
+    },
+  ]
+}
+
+function sidebarFrameword() {
+  return [
+    {
+      items: [
+        { text: 'vue3的基础概念', link: '/framework/vue3/basicConcepts' },
+        { text: '@vue/cli脚手架', link: '/framework/vue3/vue-cli' },
+        {
+          text: 'vue基础用法与模板语法',
+          link: '/framework/vue3/basicUsageAndTemplateSyntax',
+        },
+        { text: 'vue基本指令', link: '/framework/vue3/basicinStruction' },
+        { text: 'v-blid和v-on', link: '/framework/vue3/v-buildAndv-on' },
+      ],
+    },
+  ]
 }
