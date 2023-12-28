432a20829109323b581e81f79fdcef550be5fd95
mafqla <1692285748@qq.com>
2023-12-28 15:21:52 +0800

    test

--- api/proxy.js
+++ api/proxy.js
@@ -13,5 +13,15 @@ export default (req, res) => {
     pathRewrite: {
       '^/douyin/': '/'
     }
-  })(req, res)
+  })
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  return new Promise(resolve => {
+    proxy(req, res, resolve)
+  })
 }
