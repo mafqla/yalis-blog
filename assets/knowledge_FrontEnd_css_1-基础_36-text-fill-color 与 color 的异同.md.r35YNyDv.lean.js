4018226c1b1b583be8ff666bba07834772487732
mafqla <57307446+mafqla@users.noreply.github.com>
2024-01-17 06:25:51 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@88e3c17c9bc377da1c98a040e9a91ea1012407e3 🚀

--- assets/knowledge_FrontEnd_css_1-基础_36-text-fill-color 与 color 的异同.md.r35YNyDv.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as t,c as l,o as r,m as e,a as o,V as a}from"./chunks/framework.YbGCv4uC.js";const w=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/36-text-fill-color 与 color 的异同.md","filePath":"knowledge/FrontEnd/css/1-基础/36-text-fill-color 与 color 的异同.md","lastUpdated":1705472608000}'),c={name:"knowledge/FrontEnd/css/1-基础/36-text-fill-color 与 color 的异同.md"},n=e("p",null,[e("code",null,"text-fill-color"),o(" 究竟是何方神圣呢？从字面意思理解，直译就是文本填充颜色，其实它与 "),e("code",null,"color"),o(" 的作用是一样的，为文字设定颜色值。")],-1),d=e("p",null,[o("而且，"),e("strong",null,"text-fill-color 会覆盖 color 所定义的字体颜色"),o("，也就是前者的优先级更高。可以看看这个 Demo：")],-1),i=e("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"-webkit-text-fill-color && color",src:"https://codepen.io/mafqla/embed/wvOWNzp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/wvOWNzp">
-  -webkit-text-fill-color &amp;&amp; color</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),s=a("",11),p=e("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"text-stroke",src:"https://codepen.io/mafqla/embed/ZEPOwpw?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/ZEPOwpw">
-  text-stroke</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),f=[n,d,i,s,p];function h(_,m,x,u,b,k){return r(),l("div",null,f)}const S=t(c,[["render",h]]);export{w as __pageData,S as default};
