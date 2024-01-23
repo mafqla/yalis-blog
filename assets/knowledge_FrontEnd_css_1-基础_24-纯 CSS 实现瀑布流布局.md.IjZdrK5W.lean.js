219dce49838b91f3b9cc38ba8c12aede35a08e72
mafqla <57307446+mafqla@users.noreply.github.com>
2024-01-23 03:36:29 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@afd1b4d1ee88bdd38a6439d6c8f2bf22546e86b2 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_24-纯 CSS 实现瀑布流布局.md.IjZdrK5W.lean.js
@@ -0,0 +1,13 @@
+import{_ as l,c as t,o,m as e,a,V as n}from"./chunks/framework.YbGCv4uC.js";const w=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/24-纯 CSS 实现瀑布流布局.md","filePath":"knowledge/FrontEnd/css/1-基础/24-纯 CSS 实现瀑布流布局.md","lastUpdated":1705980879000}'),s={name:"knowledge/FrontEnd/css/1-基础/24-纯 CSS 实现瀑布流布局.md"},d=e("h2",{id:"css-实现瀑布流布局-display-flex",tabindex:"-1"},[a("CSS 实现瀑布流布局（display: flex） "),e("a",{class:"header-anchor",href:"#css-实现瀑布流布局-display-flex","aria-label":'Permalink to "CSS 实现瀑布流布局（display: flex）"'},"​")],-1),r=e("ul",null,[e("li",null,"本例使用 CSS flex 实现瀑布流布局"),e("li",null,"关键点，横向 flex 布局嵌套多列纵向 flex 布局，使用了 vw 进行自适应缩放")],-1),i=e("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS实现瀑布流布局（display: flex）",src:"https://codepen.io/mafqla/embed/RwdaRjO?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/RwdaRjO">
+  CSS实现瀑布流布局（display: flex）</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=e("h2",{id:"css-实现瀑布流布局-column-count",tabindex:"-1"},[a("CSS 实现瀑布流布局（column-count） "),e("a",{class:"header-anchor",href:"#css-实现瀑布流布局-column-count","aria-label":'Permalink to "CSS 实现瀑布流布局（column-count）"'},"​")],-1),h=e("ul",null,[e("li",null,"本例使用 CSS column 实现瀑布流布局"),e("li",null,[a("关键点，"),e("code",null,"column-count"),a(" -- 元素内容将被划分的最佳列数")]),e("li",null,[a("关键点，"),e("code",null,"break-inside"),a(" -- 避免在元素内部插入分页符")])],-1),p=e("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS实现瀑布流布局（column-count）",src:"https://codepen.io/mafqla/embed/xxBVOpz?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/xxBVOpz">
+  CSS实现瀑布流布局（column-count）</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),f=n("",3),_=e("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS实现瀑布流布局（display: grid）",src:"https://codepen.io/mafqla/embed/LYaNZeg?default-tab=html%2Cresult",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/LYaNZeg">
+  CSS实现瀑布流布局（display: grid）</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),m=[d,r,i,c,h,p,f,_];function u(S,C,g,y,x,b){return o(),t("div",null,m)}const P=l(s,[["render",u]]);export{w as __pageData,P as default};
--- assets/knowledge_FrontEnd_css_1-基础_42-不可思议的纯 CSS 滚动进度条效果.md.9jYAd9Ci.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as i,c as a,o as n,m as s,V as t}from"./chunks/framework.YbGCv4uC.js";const h="/assets/50879862-643c7d80-1417-11e9-8600-4133b0751626.yI_x3S2R.gif",l="/assets/50880203-871b6180-1418-11e9-8588-f0be26721fbe.ysnEXNk-.gif",e="/assets/50880270-d6619200-1418-11e9-97c7-601f7aca01e7.5n5ZMT4g.gif",p="/assets/50880349-19236a00-1419-11e9-8a85-8d14ace2ac86.hcQ03WOt.png",f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/42-不可思议的纯 CSS 滚动进度条效果.md","filePath":"knowledge/FrontEnd/css/1-基础/42-不可思议的纯 CSS 滚动进度条效果.md","lastUpdated":1705979632000}'),k={name:"knowledge/FrontEnd/css/1-基础/42-不可思议的纯 CSS 滚动进度条效果.md"},d=s("p",null,"如何使用 CSS 实现下述滚动条效果？",-1),r=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"使用线性渐变实现滚动进度条",src:"https://codepen.io/mafqla/embed/JjzRNWj?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/JjzRNWj">
-  使用线性渐变实现滚动进度条</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),B=t("",16),F=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"使用线性渐变实现滚动进度条",src:"https://codepen.io/mafqla/embed/JjzRNWj?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/JjzRNWj">
-  使用线性渐变实现滚动进度条</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),o=[d,r,B,F];function g(D,c,y,C,A,_){return n(),a("div",null,o)}const b=i(k,[["render",g]]);export{f as __pageData,b as default};
