e61e958111c9f72f55b0a7c7505ead0f7b542ab9
mafqla <57307446+mafqla@users.noreply.github.com>
2024-01-28 12:51:13 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@d5072a8390b2374dc50f695c7894819765dff7ba 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_49-巧妙实现带圆角的渐变边框.md.racs07rn.lean.js
@@ -0,0 +1,9 @@
+import{_ as e,c as h,o as n,V as s,m as a}from"./chunks/framework.YbGCv4uC.js";const l="/assets/73241946-92395f00-41de-11ea-9a7d-4e05009f6079.sQJfKXgy.png",i="/assets/73240788-007c2280-41db-11ea-8946-3de20661df85.HtF5B8P_.png",m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/49-巧妙实现带圆角的渐变边框.md","filePath":"knowledge/FrontEnd/css/1-基础/49-巧妙实现带圆角的渐变边框.md","lastUpdated":1706446163000}'),p={name:"knowledge/FrontEnd/css/1-基础/49-巧妙实现带圆角的渐变边框.md"},t=s("",20),k=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"bg + overflow 实现渐变边框",src:"https://codepen.io/mafqla/embed/QWopNzM?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/QWopNzM">
+  bg + overflow 实现渐变边框</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=s("",10),d=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background-clip 实现渐变边框",src:"https://codepen.io/mafqla/embed/ExMWKGG?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/ExMWKGG">
+  background-clip 实现渐变边框</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),B=s("",21),F=[t,k,r,d,B];function o(g,D,c,y,C,A){return n(),h("div",null,F)}const u=e(p,[["render",o]]);export{m as __pageData,u as default};
