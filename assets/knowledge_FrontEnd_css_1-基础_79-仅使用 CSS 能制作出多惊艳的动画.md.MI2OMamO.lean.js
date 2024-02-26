276419a1ae789616d82a6e4f48cb3ee2ab3bd014
mafqla <57307446+mafqla@users.noreply.github.com>
2024-02-26 02:10:15 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@3c2e2eef3207c03f9526cb9c1c898e4c27d6363c 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_79-仅使用 CSS 能制作出多惊艳的动画.md.MI2OMamO.lean.js
@@ -0,0 +1,49 @@
+import{_ as h,a as n,b as k,c as l,d as t}from"./chunks/130324868-f895a45e-a74d-44e4-8f1f-043e258507c7.eSPaOs_b.js";import{_ as p,c as e,o as F,V as i,m as s,a}from"./chunks/framework.YbGCv4uC.js";const B="/assets/129482805-ea8d1d4f-3db8-4bff-8d36-d14827dfe264.IuHY09Ex.gif",r="/assets/129483233-cc7eda2b-ab9c-49ed-9888-9a69ec58a005.ujwlw3dS.gif",D="/assets/129896052-5dc4a345-dc8d-44ee-b024-fbbf9c910627.EAs5wV3E.png",d="/assets/129896286-a07a33ea-ac39-434b-882b-3bd570de87aa.iYKeYDeU.png",g="/assets/129924158-e393ea7d-5642-4528-9be7-26e68becd660.acjm4LRn.gif",y="/assets/129924828-144a5faf-1aca-432c-a11f-ad0b34470f42.x4l2BXwe.gif",C="/assets/129925324-717d8916-f243-4255-9a34-3a9ccd867381.on7JOCY2.gif",A="/assets/130071221-523eb632-0be7-4a81-b3a7-00d38a2817af.z8_k9Qng.png",o="/assets/130071967-d1cfbdb5-41c1-485f-86eb-e6c269173aff.4FKqZm7E.png",c="/assets/130072606-db0e6aea-8d26-4ebe-a966-55bf79b23431.w08kEimS.png",E="/assets/130097323-3907beaa-97e3-4c18-96c7-15d31e3807cb.1hBu3FKR.gif",m="/assets/130098863-6501683a-9e21-4b0f-bcca-228f153fed56.3VtAmX5J.gif",f="/assets/130098300-8579ca1c-8f9e-47ca-beb9-4bfdd9ee2c22.RAzoue5C.gif",u="/assets/130341829-93bee21c-c7bd-46fd-9abc-f6198d1b1d39.GO_Oi79K.gif",b="/assets/130445668-eb817128-d055-4dcb-a73e-37376cc7a48f.V0WgY_KQ.png",Bs=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/79-仅使用 CSS 能制作出多惊艳的动画.md","filePath":"knowledge/FrontEnd/css/1-基础/79-仅使用 CSS 能制作出多惊艳的动画.md","lastUpdated":1708913260000}'),_={name:"knowledge/FrontEnd/css/1-基础/79-仅使用 CSS 能制作出多惊艳的动画.md"},v=i("",26),q=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Webpack logo",src:"https://codepen.io/mafqla/embed/RwdmXKZ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/RwdmXKZ">
+  Webpack logo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),S=i("",20),x=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Neon Effect 3D TEXT",src:"https://codepen.io/mafqla/embed/KKELOaJ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/KKELOaJ">
+  Neon Effect 3D TEXT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),T=i("",7),P=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Glowing 3D  TEXT",src:"https://codepen.io/mafqla/embed/MWxdNJM?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/MWxdNJM">
+  Glowing 3D  TEXT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),w=i("",16),Z=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS 3D TEXT",src:"https://codepen.io/mafqla/embed/NWJVQgO?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/NWJVQgO">
+  CSS 3D TEXT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),z=i("",62),X=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS Galaxy  Shuttle",src:"https://codepen.io/mafqla/embed/Yzgbmxx?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/Yzgbmxx">
+  Pure CSS Galaxy  Shuttle</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),V=i("",6),N=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Perspective Origin Tunnel Animation",src:"https://codepen.io/mafqla/embed/OJqYKjE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/OJqYKjE">
+  Perspective Origin Tunnel Animation</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),G=i("",9),O=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS 3D parallax",src:"https://codepen.io/mafqla/embed/ZEPNgXz?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/ZEPNgXz">
+  CSS 3D parallax</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),R=s("p",null,"很明显，当滚动滚动条时，不同子元素的位移程度从视觉上看是不一样的，也就达到了所谓的滚动视差效果。",-1),Y=s("h3",{id:"滚动视差文字阴影-虚影效果-多图展示",tabindex:"-1"},[a("滚动视差文字阴影/虚影效果/多图展示 "),s("a",{class:"header-anchor",href:"#滚动视差文字阴影-虚影效果-多图展示","aria-label":'Permalink to "滚动视差文字阴影/虚影效果/多图展示"'},"​")],-1),I=s("p",null,"那么，运用 translate3d 的视差效果，又能有一些什么好玩的效果呢？下面这个滚动视差文字阴影/虚影效果很有意思：",-1),W=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS translate3d Parallax",src:"https://codepen.io/mafqla/embed/XWGwveb?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWGwveb">
+  CSS translate3d Parallax</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),$=s("p",null,"另外一种就是我们可以把这个技巧运用到类似个人主页，图片展示等一些大屏场景下。",-1),J=s("p",null,[a("核心就是给每张图片设置不同的 "),s("code",null,"translateZ"),a("，给父元素设置一个 "),s("code",null,"persepective"),a(" 即可，这样，在上下滚动的过程中，就能出现简单的视差效果：")],-1),K=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Scroll Parallax Effect ",src:"https://codepen.io/mafqla/embed/abMreLN?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/abMreLN">
+  CSS Scroll Parallax Effect </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),L=s("p",null,"同理，这个滚动视差不仅仅可以作用于上下的滚动，对于左右方向的滚动也是同样生效的：",-1),j=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS-Only Horizontal Parallax Gallery",src:"https://codepen.io/mafqla/embed/JjzqgrR?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/JjzqgrR">
+  CSS-Only Horizontal Parallax Gallery</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),M=i("",6),Q=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Only CSS: 404 Rolling Box",src:"https://codepen.io/mafqla/embed/dyrExVW?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/dyrExVW">
+  Only CSS: 404 Rolling Box</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),H=i("",16),U=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS 3D Progress Bar",src:"https://codepen.io/mafqla/embed/xxBNvPO?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/xxBNvPO">
+  Pure CSS 3D Progress Bar</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),ss=s("blockquote",null,[s("p",null,[a("对于 CSS "),s("a",{href:"https://github.com/Property",target:"_blank",rel:"noreferrer"},"@Property"),a(" 不算很了解的，可以看看作者的这篇文章 -- "),s("a",{href:"https://github.com/chokcoco/iCSS/issues/109",target:"_blank",rel:"noreferrer"},"CSS @property，让不可能变可能"),a("，它的出现，让 CSS 极大的提升了制作各种动画的能力。")])],-1),is=[v,q,S,x,T,P,w,Z,z,X,V,N,G,O,R,Y,I,W,$,J,K,L,j,M,Q,H,U,ss];function as(hs,ns,ks,ls,ts,ps){return F(),e("div",null,is)}const rs=p(_,[["render",as]]);export{Bs as __pageData,rs as default};
