c9946e786da441211455d7f2f8dced9c317cae04
mafqla <57307446+mafqla@users.noreply.github.com>
2024-02-27 01:04:49 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@ad55a967683aa48e3afdc974ee0898c3accd1b8f 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_39-神奇的选择器 focus-within.md.ScDMuIYj.lean.js
@@ -0,0 +1,29 @@
+import{_ as n,c as h,o as t,V as a,m as s,a as i}from"./chunks/framework.YbGCv4uC.js";const z=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/39-神奇的选择器 focus-within.md","filePath":"knowledge/FrontEnd/css/1-基础/39-神奇的选择器 focus-within.md","lastUpdated":1708995771000}'),l={name:"knowledge/FrontEnd/css/1-基础/39-神奇的选择器 focus-within.md"},p=a("",17),e=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:":focus-within 冒泡触发",src:"https://codepen.io/mafqla/embed/GReqzMv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/GReqzMv">
+  :focus-within 冒泡触发</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),k=a("",5),d=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS focus-within INPUT",src:"https://codepen.io/mafqla/embed/jOJrdGe?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/jOJrdGe">
+  CSS focus-within INPUT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=a("",5),B=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS focus-within INPUT",src:"https://codepen.io/mafqla/embed/jOJrdGe?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/jOJrdGe">
+  CSS focus-within INPUT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),F=s("p",null,"运用上面思想，我们可以把效果做的更炫一点点，在某些场景制作一些增强用户体验的效果：",-1),o=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"PURE CSS FOCUS By :focus-within",src:"https://codepen.io/mafqla/embed/eYXzxGa?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/eYXzxGa">
+  PURE CSS FOCUS By :focus-within</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),D=s("h2",{id:"tab导航切换",tabindex:"-1"},[i("TAB导航切换 "),s("a",{class:"header-anchor",href:"#tab导航切换","aria-label":'Permalink to "TAB导航切换"'},"​")],-1),c=s("p",null,[i("现在又多了一种方式，利用了 "),s("code",null,":focus-within"),i(" 可以在父节点获取元素获得焦点的特性，实现的TAB导航切换：")],-1),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"focus-within switch tab",src:"https://codepen.io/mafqla/embed/dyrXaZp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/dyrXaZp">
+  focus-within switch tab</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=a("",10),A=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:":placeholder-shown && :focus-within",src:"https://codepen.io/mafqla/embed/NWJrowz?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/NWJrowz">
+  :placeholder-shown &amp;&amp; :focus-within</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=s("p",null,"可以看到，上面的效果没有用到任何 JS，可以实现：",-1),u=s("ol",null,[s("li",null,"整个 input（包括父元素所在区域）获焦与非获焦样式控制"),s("li",null,"placeholder 属性设置的文字出现与消失后样式控制")],-1),f=s("h2",{id:"实现掘金登录动效切换",tabindex:"-1"},[i("实现掘金登录动效切换 "),s("a",{class:"header-anchor",href:"#实现掘金登录动效切换","aria-label":'Permalink to "实现掘金登录动效切换"'},"​")],-1),m=s("div",{class:"tip custom-block"},[s("p",{class:"custom-block-title"},"TIP"),s("p",null,"当前代码已经过时")],-1),E=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"掘金登录效果纯CSS实现",src:"https://codepen.io/mafqla/embed/zYbBepY?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/zYbBepY">
+  掘金登录效果纯CSS实现</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),_=[p,e,k,d,r,B,F,o,D,c,g,y,A,C,u,f,m,E];function b(w,q,S,v,P,T){return t(),h("div",null,_)}const I=n(l,[["render",b]]);export{z as __pageData,I as default};
