ec1abd6f169b16b4c5721e959004633ca2a86ee7
mafqla <57307446+mafqla@users.noreply.github.com>
2024-03-06 01:05:06 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@a1c05d92c98725de6b2c7fd2d5d4043db2ffa53f 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_59-水平垂直居中深入挖掘.md.QH2BOxsp.lean.js
@@ -0,0 +1,17 @@
+import{_ as e,c as t,o as n,V as i,m as s,a}from"./chunks/framework.bW6FiXxS.js";const S=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/59-水平垂直居中深入挖掘.md","filePath":"knowledge/FrontEnd/css/1-基础/59-水平垂直居中深入挖掘.md","lastUpdated":1709686941000}'),l={name:"knowledge/FrontEnd/css/1-基础/59-水平垂直居中深入挖掘.md"},h=i("",6),p=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Centering in CSS",src:"https://codepen.io/mafqla/embed/bGZYNzY?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/bGZYNzY">
+  Centering in CSS</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),k=i("",6),d=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Centering in CSS 2",src:"https://codepen.io/mafqla/embed/KKEywJe?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/KKEywJe">
+  Centering in CSS 2</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=i("",7),r=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Centering in CSS 2",src:"https://codepen.io/mafqla/embed/MWxOYLP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/MWxOYLP">
+  Centering in CSS 2</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("ol",null,[s("li",null,[s("code",null,"margin: auto"),a(" 由于需要均分剩余空间，所以表现并不好，无法按照我们设想的 "),s("code",null,"5px"),a(" 宽度进行间隔")])],-1),g=s("h3",{id:"让元素多到溢出",tabindex:"-1"},[a("让元素多到溢出 "),s("a",{class:"header-anchor",href:"#让元素多到溢出","aria-label":'Permalink to "让元素多到溢出"'},"​")],-1),F=s("p",null,"OK，接下来，我们让内容再多一点，多到溢出整个容器，看看有什么不一样。",-1),B=s("p",null,[s("a",{href:"https://user-images.githubusercontent.com/8554143/103790280-f534a680-507b-11eb-9ecf-138b7280f087.png",target:"_blank",rel:"noreferrer"},[s("img",{src:"https://user-images.githubusercontent.com/8554143/103790280-f534a680-507b-11eb-9ecf-138b7280f087.png",alt:"image",loading:"lazy"})])],-1),D=s("p",null,"再来一张竖直方向排列的：",-1),y=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Centering in CSS 4",src:"https://codepen.io/mafqla/embed/mdoqyoO?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/mdoqyoO">
+  Centering in CSS 4</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=i("",7),_=[h,p,k,d,o,r,c,g,F,B,D,y,C];function u(m,A,f,b,q,E){return n(),t("div",null,_)}const x=e(l,[["render",u]]);export{S as __pageData,x as default};
