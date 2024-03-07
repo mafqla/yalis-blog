79f6649b7f6415a7a1af0b19c385037664306b41
mafqla <57307446+mafqla@users.noreply.github.com>
2024-03-07 08:18:46 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@07f98c5505e24b61879dab83cd131684fb811d70 🚀

--- assets/knowledge_FrontEnd_css_1-基础_33-fixed 定位失效.md.j0LvbWh8.lean.js
+++
@@ -1,5 +0,0 @@
-import{_ as s,c as a,o as n,L as i,j as t}from"./chunks/framework.HB-xfCmj.js";import"./app.pHtHZ9Jo.js";import"./chunks/theme.ykc7KowD.js";const e="/assets/29648435-b2a9940a-88c0-11e7-9b7b-1ca69e371921.QV0O0lfb.png",f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/33-fixed 定位失效.md","filePath":"knowledge/FrontEnd/css/1-基础/33-fixed 定位失效.md","lastUpdated":1709799342000}'),l={name:"knowledge/FrontEnd/css/1-基础/33-fixed 定位失效.md"},p=i("",19),h=t("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"层叠上下文对 fixed 定位的影响（基于不同浏览器内核结果不同）",src:"https://codepen.io/mafqla/embed/YzgqRzL?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/YzgqRzL">
-  层叠上下文对 fixed 定位的影响（基于不同浏览器内核结果不同）</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),k=i("",13),o=[p,h,k];function d(r,F,c,B,g,D){return n(),a("div",null,o)}const x=s(l,[["render",d]]);export{f as __pageData,x as default};
