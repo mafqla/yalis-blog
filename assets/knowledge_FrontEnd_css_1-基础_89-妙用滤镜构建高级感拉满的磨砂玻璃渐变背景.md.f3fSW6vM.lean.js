dfd80bf0a04588d8ef6de205ee3a8cc4dd22fc7a
mafqla <57307446+mafqla@users.noreply.github.com>
2024-03-07 00:43:35 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@8ebbfbc885310722a1e15a26993c3ad3cec46810 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_89-妙用滤镜构建高级感拉满的磨砂玻璃渐变背景.md.f3fSW6vM.lean.js
@@ -0,0 +1,9 @@
+import{_ as a,c as n,o as h,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const l="/assets/148382783-56f530a1-77f2-4a74-b68e-7ab4a94cc74f.n5t_R0Po.png",t="/assets/148385057-b6704fdc-779b-43a7-9508-2b9e9e412a46.fIficIY_.gif",k="/assets/148385294-0d0a0635-6557-4c99-a50b-873e9efe75fa.BDVfAiBL.gif",E=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/89-妙用滤镜构建高级感拉满的磨砂玻璃渐变背景.md","filePath":"knowledge/FrontEnd/css/1-基础/89-妙用滤镜构建高级感拉满的磨砂玻璃渐变背景.md","lastUpdated":1709772090000}'),p={name:"knowledge/FrontEnd/css/1-基础/89-妙用滤镜构建高级感拉满的磨砂玻璃渐变背景.md"},e=s("",13),B=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Frosted glass background effect",src:"https://codepen.io/mafqla/embed/XWQWPmE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWQWPmE">
+  Frosted glass background effect</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=s("",12),F=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS-doodle Pure CSS Background Effect  2",src:"https://codepen.io/mafqla/embed/KKYKxdm?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/KKYKxdm">
+  CSS-doodle Pure CSS Background Effect  2</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),d=[e,B,r,F];function D(g,y,o,C,A,c){return h(),n("div",null,d)}const m=a(p,[["render",D]]);export{E as __pageData,m as default};
