5dc8ca71632487615490ad1c1839948d2ba1ca45
mafqla <57307446+mafqla@users.noreply.github.com>
2024-03-14 14:29:46 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@715c0d9458c76e279711d96f332db7ff4ae58a8d 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_92-巧用 CSS 实现炫彩三角边框动画.md.1eTELc2Y.lean.js
@@ -0,0 +1,13 @@
+import{_ as a,c as h,o as n,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const k="/assets/153804505-ac45045b-bfe9-4017-adb6-b2a878aa578f.E__F09A5.gif",l="/assets/153804910-f7ecb787-7bef-4dc5-b5d6-e92a18b1bbcf.9o3a6B1E.gif",p="/assets/153805987-f4d39264-28fa-47b9-966a-7e264d106613.5wI-vznT.gif",t="/assets/153806660-cb4923db-6fa3-41d5-86a8-ad28e172e476.Dm0_yha-.gif",e="/assets/154028267-d808ddae-bcdb-46c8-849f-425aa797a6d3.3G6FOWyX.png",F="/assets/153807202-944a5107-592f-499f-99ec-d5557cae5e9e.DegBWGEd.gif",B="/assets/154185557-61627314-d29f-4fcf-956c-6a45058fa7f9._AMolcO1.png",r="/assets/154186020-d9ef91e9-7d02-4e39-ba5d-45092b0da25a.T6Uwo37M.png",w=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/92-巧用 CSS 实现炫彩三角边框动画.md","filePath":"knowledge/FrontEnd/css/1-基础/92-巧用 CSS 实现炫彩三角边框动画.md","lastUpdated":1710426466000}'),d={name:"knowledge/FrontEnd/css/1-基础/92-巧用 CSS 实现炫彩三角边框动画.md"},g=s("",22),C=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS Linear Triangle",src:"https://codepen.io/mafqla/embed/yLrYNqq?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/yLrYNqq">
+  Pure CSS Linear Triangle</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),D=s("",20),y=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS Glowing Triangle",src:"https://codepen.io/mafqla/embed/qBwOdMW?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/qBwOdMW">
+  Pure CSS Glowing Triangle</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),A=s("",6),c=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS Glowing Triangle 2",src:"https://codepen.io/mafqla/embed/PogPqdp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/PogPqdp">
+  Pure CSS Glowing Triangle 2</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=s("",10),E=[g,C,D,y,A,c,o];function f(m,_,u,b,v,S){return n(),h("div",null,E)}const q=a(d,[["render",f]]);export{w as __pageData,q as default};
