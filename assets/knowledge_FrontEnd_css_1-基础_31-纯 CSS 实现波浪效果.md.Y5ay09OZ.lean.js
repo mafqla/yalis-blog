bd9aaf8b8b5d59f1246f8720f452afc793b6b98a
mafqla <57307446+mafqla@users.noreply.github.com>
2024-03-29 01:22:20 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@5f2145c6517db3ebb8538a867b2762ce31e4bc99 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_31-纯 CSS 实现波浪效果.md.Y5ay09OZ.lean.js
@@ -0,0 +1,21 @@
+import{_ as a,c as h,o as k,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const n="/assets/28254807-e89e59ca-6ae2-11e7-90a2-95fc45177fb6.QhiiqUBj.png",t="/assets/28255610-c02c7bc6-6aeb-11e7-95f0-6f7393f5698b._RPJFxFF.gif",l="/assets/28261913-c1757820-6b13-11e7-9cdc-32cb75331b03.46OQ5htU.png",p="/assets/28261931-cf981944-6b13-11e7-8162-2bd5d2d2d77e.OkuTi8cJ.png",e="/assets/28262766-ddd5c742-6b16-11e7-81da-c238ff92955d.mT7hHh6l.gif",F="/assets/28265378-f9bf3f82-6b21-11e7-83eb-e240b40b550a.s022z8OG.gif",B="/assets/28265844-61b7a690-6b24-11e7-91c7-c5b8501487ab.oMgP3Y1C.gif",P=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/31-纯 CSS 实现波浪效果.md","filePath":"knowledge/FrontEnd/css/1-基础/31-纯 CSS 实现波浪效果.md","lastUpdated":1711675208000}'),r={name:"knowledge/FrontEnd/css/1-基础/31-纯 CSS 实现波浪效果.md"},D=s("",11),d=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"SVG WAVE",src:"https://codepen.io/mafqla/embed/OJqNBqa?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/OJqNBqa">
+  SVG WAVE</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=s("",5),y=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Canvas Wave",src:"https://codepen.io/mafqla/embed/PoLNyvx?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/PoLNyvx">
+  Canvas Wave</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=s("",24),A=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure Css Wave",src:"https://codepen.io/mafqla/embed/JjzXmQJ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/JjzXmQJ">
+  Pure Css Wave</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=s("",7),c=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"pure css wave",src:"https://codepen.io/mafqla/embed/LYaNgKz?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/LYaNgKz">
+  pure css wave</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),E=s("",7),u=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure Css Wave Progress bar",src:"https://codepen.io/mafqla/embed/RwdaezJ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/RwdaezJ">
+  Pure Css Wave Progress bar</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),m=s("",4),f=[D,d,g,y,C,A,o,c,E,u,m];function q(_,b,v,S,w,x){return k(),h("div",null,f)}const V=a(r,[["render",q]]);export{P as __pageData,V as default};
