20012cb15c2afdba4d5b2d0fa31ab0cf68bb7ed7
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-07 00:59:42 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@cfb673f3bfab9ed772dc830aed142de03858bb4c 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_109-使用 CSS 构建强大且酷炫的粒子动画.md.Qvk5wLni.lean.js
@@ -0,0 +1,25 @@
+import{_ as e,c as n,o as t,V as i,m as s,a}from"./chunks/framework.bW6FiXxS.js";const l="/assets/179983096-0a3798d6-2955-45b0-a277-bbcc12c5c50c.D7FuP3q7.png",p="/assets/179983990-0a6829f4-4ccd-4dd1-954f-86916a10a40d.aXQ7_RFN.png",h="/assets/179984739-fe9d947b-efb8-41f7-8729-ab27cc640570.Yg5c2Qta.png",r="/assets/179985920-a13161df-42a5-4ebe-b650-e41241ccaa03.KIDc3bdN.gif",o="/assets/179989073-d67c6a40-d247-4e57-8117-bb9dec62f823.BjfdJC43.png",d="/assets/179989673-99e73ee1-f15b-4916-8359-863990e3ae86.34k7LNjx.gif",V=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/109-使用 CSS 构建强大且酷炫的粒子动画.md","filePath":"knowledge/FrontEnd/css/1-基础/109-使用 CSS 构建强大且酷炫的粒子动画.md","lastUpdated":1712451445000}'),k={name:"knowledge/FrontEnd/css/1-基础/109-使用 CSS 构建强大且酷炫的粒子动画.md"},c=i("",30),B=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Doodle - CSS Pattern ",src:"https://codepen.io/mafqla/embed/JjVrKJP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/JjVrKJP">
+  CSS Doodle - CSS Pattern </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=s("p",null,[a("当然，我们完全可以换一个配色，黑色底色配合上 ，让每一个元素发光发亮，这样，我们就得到了这样一个效果："),s("code",null,"box-shadow()")],-1),F=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Doodle - CSS Pattern ",src:"https://codepen.io/mafqla/embed/dyLVXRY?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/dyLVXRY">
+  CSS Doodle - CSS Pattern </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),m=i("",13),D=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Doodle - CSS Pattern Effect",src:"https://codepen.io/mafqla/embed/GRLMqEr?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/GRLMqEr">
+  CSS Doodle - CSS Pattern Effect</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=s("p",null,[a("可以看到，利用"),s("strong",null,"柏林噪声"),a("生成的粒子效果，更加的真实，看上去更加的带感。")],-1),f=s("p",null,"当然，掌握了这个技巧之后，我们可以尝试其他添加其他属性的动画，那么可能我们会得到这样的动画：",-1),A=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Doodle - CSS Pattern ",src:"https://codepen.io/mafqla/embed/yLrzJXx?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/yLrzJXx">
+  CSS Doodle - CSS Pattern </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),u=i("",3),C=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Doodle - CSS Pattern ",src:"https://codepen.io/mafqla/embed/XWQeKgG?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWQeKgG">
+  CSS Doodle - CSS Pattern </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),_=s("p",null,"CSS-Doodle 库的作者，袁川老师，也有非常多 3D 粒子动画，其中一幅：",-1),b=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Seeding",src:"https://codepen.io/mafqla/embed/qBwPNXB?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/qBwPNXB">
+  Seeding</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),S=[c,B,g,F,m,D,y,f,A,u,C,_,b];function v(E,q,x,w,P,z){return t(),n("div",null,S)}const N=e(k,[["render",v]]);export{V as __pageData,N as default};
