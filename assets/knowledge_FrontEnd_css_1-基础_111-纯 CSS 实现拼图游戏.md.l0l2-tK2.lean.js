b12f00c6a067bf1fa1080344f9b9ed27524b8bf1
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-08 01:50:59 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@31c56829cffb1aef3dc8f1e8c5dda12ecb0a1488 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_111-纯 CSS 实现拼图游戏.md.l0l2-tK2.lean.js
@@ -0,0 +1,13 @@
+import{_ as a,c as h,o as n,V as i,m as s}from"./chunks/framework.bW6FiXxS.js";const k="/assets/182604075-791ca1e8-1eac-4379-b8e9-46a03454a517.1CgQXBWs.png",l="/assets/182607097-e9ac86cf-1063-454d-b956-93c40d49e10e.ldLKZHNB.gif",t="/assets/182608383-6a407d8b-5785-415f-a6a6-36758ee54f42.A_2yYxJv.gif",p="/assets/182609475-b70d8067-21be-4ca4-8289-8851c2a37bd3.WiJN-L96.gif",e="/assets/182612447-d3cf9b4c-12de-49de-a5a1-49324c0a6f7b.QV1k4bhu.gif",F="/assets/182614416-91c834a8-2d0e-4b10-8f72-67e5e9ee95e6.1cvRin1Y.gif",q=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/111-纯 CSS 实现拼图游戏.md","filePath":"knowledge/FrontEnd/css/1-基础/111-纯 CSS 实现拼图游戏.md","lastUpdated":1712540935000}'),B={name:"knowledge/FrontEnd/css/1-基础/111-纯 CSS 实现拼图游戏.md"},D=i("",8),r=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"HTML draggable Demo",src:"https://codepen.io/mafqla/embed/vYMWzMd?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/vYMWzMd">
+  HTML draggable Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),d=i("",35),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"HTML draggable Demo",src:"https://codepen.io/mafqla/embed/XWQzPQd?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWQzPQd">
+  HTML draggable Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=i("",6),C=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Pure CSS Spelling game",src:"https://codepen.io/mafqla/embed/NWmwLJo?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/NWmwLJo">
+  Pure CSS Spelling game</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),A=s("p",null,"在上述的代码中，我们利用了 SASS 快速实现了不同块的文字的位置的随机摆放，增加一定的随机性。同时，利用 SASS 减少了一些重复性代码的工作量。",-1),o=[D,r,d,g,y,C,A];function c(E,u,m,b,v,f){return n(),h("div",null,o)}const x=a(B,[["render",c]]);export{q as __pageData,x as default};
