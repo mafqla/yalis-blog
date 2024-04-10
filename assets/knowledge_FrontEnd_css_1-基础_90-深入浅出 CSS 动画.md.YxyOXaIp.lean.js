99c3f68a965919974f07a99093faa397f1102ca4
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-10 06:59:32 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@2adc8515d0fa4e1f52adcfde879aa180a608cf91 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_90-深入浅出 CSS 动画.md.YxyOXaIp.lean.js
@@ -0,0 +1,25 @@
+import{_ as a,c as n,o as t,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const h="/assets/137946058-cd27fdc0-45a6-4710-a5bd-f81a9ee77bd4.TuRz29SP.gif",q=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/90-深入浅出 CSS 动画.md","filePath":"knowledge/FrontEnd/css/1-基础/90-深入浅出 CSS 动画.md","lastUpdated":1712732238000}'),l={name:"knowledge/FrontEnd/css/1-基础/90-深入浅出 CSS 动画.md"},e=s("",55),p=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"HuaWei Battery Charging Animation",src:"https://codepen.io/mafqla/embed/mdgygRZ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/mdgygRZ">
+  HuaWei Battery Charging Animation</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),k=s("",8),B=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Aniamtion Demo",src:"https://codepen.io/mafqla/embed/wvZBZJK?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/wvZBZJK">
+  Aniamtion Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=s("",19),d=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Tick Tock",src:"https://codepen.io/mafqla/embed/rNbabyY?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/rNbabyY">
+  Tick Tock</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),F=s("",16),o=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Sprite Animation with steps() ",src:"https://codepen.io/mafqla/embed/xxebeqz?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/xxebeqz">
+  Sprite Animation with steps() </a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=s("",22),D=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Scale Loading steps vs linear",src:"https://codepen.io/mafqla/embed/RwONOpm?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/RwONOpm">
+  Scale Loading steps vs linear</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("",14),y=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS BEER!",src:"https://codepen.io/mafqla/embed/BaEyERN?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/BaEyERN">
+  CSS BEER!</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),A=s("",114),C=[e,p,k,B,r,d,F,o,g,D,c,y,A];function m(f,u,b,E,v,S){return t(),n("div",null,C)}const x=a(l,[["render",m]]);export{q as __pageData,x as default};
