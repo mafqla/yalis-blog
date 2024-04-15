73ab6c6cbe3f292eb22bfda491980a33c12a3886
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-15 01:09:35 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@50f1af52902ce85f9482cf237e3e2d4691fb578b 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_66-奇思妙想 CSS 文字动画.md.A_4silIg.lean.js
@@ -0,0 +1,61 @@
+import{_ as h}from"./chunks/109828417-948aaa00-7c77-11eb-876f-d2cd863b74ac.ZmwlLMCH.js";import{_ as k,c as n,o as l,V as i,m as s,a}from"./chunks/framework.bW6FiXxS.js";const p="/assets/108831662-65d05c00-7605-11eb-9e65-fe736c8d809c.KZm5QRhn.png",t="/assets/109823628-13311880-7c73-11eb-8b2a-aa82968a4cea.ph4ffEZx.png",e="/assets/108867519-413da980-7630-11eb-8a81-27751428be82.0JKwuU19.png",F="/assets/108867906-a2fe1380-7630-11eb-9210-bbfcaac61afa.xqrNPboV.png",B="/assets/108870783-6d0e5e80-7633-11eb-9573-b73a9ea3309a.niQ7tbPV.png",r="/assets/109021471-e1113b00-76f5-11eb-9070-d3959967a385._VzJ2DHf.png",d="/assets/109511001-9882c480-7add-11eb-9f29-9a47348b52f4.qSt-b3u4.png",D="/assets/109512777-68d4bc00-7adf-11eb-9162-256f3c8d02d3.V34r4CsR.png",g="/assets/109513382-ff08e200-7adf-11eb-83f0-82b5b1f5dc40.1daZkBQe.png",C="/assets/109516773-899f1080-7ae3-11eb-9c02-3bdac03200fc.htkSEHwH.png",y="/assets/109518177-fbc42500-7ae4-11eb-80dd-8fea54ee6760.q-0oYuNB.gif",ls=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/66-奇思妙想 CSS 文字动画.md","filePath":"knowledge/FrontEnd/css/1-基础/66-奇思妙想 CSS 文字动画.md","lastUpdated":1713143248000}'),A={name:"knowledge/FrontEnd/css/1-基础/66-奇思妙想 CSS 文字动画.md"},o=i("",32),c=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"5 text shadow effects in css3",src:"https://codepen.io/mafqla/embed/xxBjaeP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/xxBjaeP">
+  5 text shadow effects in css3</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),E=i("",8),m=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Neon Demo",src:"https://codepen.io/mafqla/embed/LYamJvd?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/LYamJvd">
+  Neon Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),f=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Neon Glow",src:"https://codepen.io/FelixRilling/embed/oNNLMb?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/FelixRilling/pen/oNNLMb">
+  Neon Glow</a> by Felix Rilling (<a href="https://codepen.io/FelixRilling">@FelixRilling</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),b=i("",3),u=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Neon Demo",src:"https://codepen.io/mafqla/embed/VwRxGNB?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/VwRxGNB">
+  Neon Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),x=i("",9),_=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background-clip: unset;",src:"https://codepen.io/mafqla/embed/gOEzdyE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/gOEzdyE">
+  background-clip: unset;</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),v=i("",4),q=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background-clip:text",src:"https://codepen.io/mafqla/embed/NWJMLVW?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/NWJMLVW">
+  background-clip:text</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),S=i("",4),w=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"clip:text && color: transparent",src:"https://codepen.io/mafqla/embed/jOJxvob?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/jOJxvob">
+  clip:text &amp;&amp; color: transparent</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),T=s("p",null,[a("通过将文字设置为透明，原本 div 的背景就显现出来了，而文字以外的区域全部被裁剪了，这就是 "),s("code",null,"background-clip:text"),a(" 的作用。")],-1),P=s("h3",{id:"利用-background-clip-图文搭配",tabindex:"-1"},[a("利用 "),s("code",null,"background-clip"),a(" 图文搭配 "),s("a",{class:"header-anchor",href:"#利用-background-clip-图文搭配","aria-label":'Permalink to "利用 `background-clip` 图文搭配"'},"​")],-1),V=s("p",null,"这样，我们可以选取合适的图片合适的字体，实现任意风格的文字效果。",-1),z=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background-clip: text & Image text",src:"https://codepen.io/mafqla/embed/poYVOmy?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/poYVOmy">
+  background-clip: text &amp; Image text</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),N=i("",7),R=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background-clip: text 文字渐变色",src:"https://codepen.io/mafqla/embed/eYXrLag?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/eYXrLag">
+  background-clip: text 文字渐变色</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),I=i("",8),L=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Shine Text",src:"https://codepen.io/mafqla/embed/ExMLezv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/ExMLezv">
+  Shine Text</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),O=i("",7),G=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"MASK Text Effect",src:"https://codepen.io/mafqla/embed/xxBjaNP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/xxBjaNP">
+  MASK Text Effect</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),$=i("",18),j=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Hollow TEXT EFFECT",src:"https://codepen.io/mafqla/embed/gOEzdJE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/gOEzdJE">
+  Hollow TEXT EFFECT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),M=i("",15),J=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Hollow TEXT EFFECT",src:"https://codepen.io/mafqla/embed/QWorVXb?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/QWorVXb">
+  Hollow TEXT EFFECT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),W=i("",7),X=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Radial-gradient + Mix-blend-mode",src:"https://codepen.io/mafqla/embed/jOJxvjb?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/jOJxvjb">
+  Radial-gradient + Mix-blend-mode</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),H=i("",12),Y=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"SVG Text Line Effect",src:"https://codepen.io/mafqla/embed/XWGqPLg?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWGqPLg">
+  SVG Text Line Effect</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),K=[o,c,E,m,f,b,u,x,_,v,q,S,w,T,P,V,z,N,R,I,L,O,G,$,j,M,J,W,X,H,Y];function Q(U,Z,ss,is,as,hs){return l(),n("div",null,K)}const ps=k(A,[["render",Q]]);export{ls as __pageData,ps as default};
