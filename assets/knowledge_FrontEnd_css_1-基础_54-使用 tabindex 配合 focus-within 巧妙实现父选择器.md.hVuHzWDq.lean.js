4365dfa12730d9e0b7259b65a5409f21f610ff82
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-16 01:03:06 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@81239ec5950729e8aba6b843f3c515e7744bdb41 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_54-使用 tabindex 配合 focus-within 巧妙实现父选择器.md.hVuHzWDq.lean.js
@@ -0,0 +1,13 @@
+import{_ as a,c as t,o as e,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const n="/assets/97591748-e2c63d00-1a3a-11eb-90f2-bd2609a57b8b.AIaZaqip.gif",h="/assets/97591792-efe32c00-1a3a-11eb-86fa-295ac87483cb.CkOGvsgt.gif",l="/assets/97651574-69a90300-1a97-11eb-84f9-92c7bb00439f.-5G33aAh.gif",_=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/54-使用 tabindex 配合 focus-within 巧妙实现父选择器.md","filePath":"knowledge/FrontEnd/css/1-基础/54-使用 tabindex 配合 focus-within 巧妙实现父选择器.md","lastUpdated":1713229249000}'),p={name:"knowledge/FrontEnd/css/1-基础/54-使用 tabindex 配合 focus-within 巧妙实现父选择器.md"},k=s("",9),o=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS focus-within INPUT",src:"https://codepen.io/mafqla/embed/bGZRWqX?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/bGZRWqX">
+  CSS focus-within INPUT</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),d=s("",15),r=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"tabindex 配合 focus-within 实现div的父选择器",src:"https://codepen.io/mafqla/embed/ZEPyKKp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/ZEPyKKp">
+  tabindex 配合 focus-within 实现div的父选择器</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("",13),F=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"button 的 focus 事件冒泡性验证（Chorme / Safari / Firefox）",src:"https://codepen.io/mafqla/embed/XWGgRRM?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWGgRRM">
+  button 的 focus 事件冒泡性验证（Chorme / Safari / Firefox）</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=[k,o,d,r,c,F];function B(D,u,f,y,C,b){return e(),t("div",null,g)}const m=a(p,[["render",B]]);export{_ as __pageData,m as default};
