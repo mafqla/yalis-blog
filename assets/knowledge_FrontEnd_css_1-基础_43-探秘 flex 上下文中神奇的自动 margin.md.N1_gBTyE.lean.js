155fcc83b7305f19fde314aa607cda346a5c49b0
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-19 00:49:05 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@f393eef1ed072f9dc4bd98a0dfef11e732dcdf5e 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_43-探秘 flex 上下文中神奇的自动 margin.md.N1_gBTyE.lean.js
@@ -0,0 +1,29 @@
+import{_ as a,c as n,o as l,V as i,m as s,a as t}from"./chunks/framework.bW6FiXxS.js";const h="/assets/58150482-4faba400-7c99-11e9-8f87-074530e4c0b3.V4qVVWHJ.png",e="/assets/58150444-360a5c80-7c99-11e9-9210-d31c4435a22a.bm5Wvkdj.png",p="/assets/58173850-f12bf900-7c8b-11e9-86e0-b739286fafa6.g03Djw0i.gif",V=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/43-探秘 flex 上下文中神奇的自动 margin.md","filePath":"knowledge/FrontEnd/css/1-基础/43-探秘 flex 上下文中神奇的自动 margin.md","lastUpdated":1713487609000}'),k={name:"knowledge/FrontEnd/css/1-基础/43-探秘 flex 上下文中神奇的自动 margin.md"},B=i("",39),r=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"margin auto 实现 flex 下的 space-around",src:"https://codepen.io/Chokcoco/embed/rgYOVo?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/rgYOVo">
+  margin auto 实现 flex 下的 space-around</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),F=i("",14),d=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"margin auto 实现 flex 下的 align-self: center",src:"https://codepen.io/Chokcoco/embed/oRobEp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/oRobEp">
+  margin auto 实现 flex 下的 align-self: center</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"margin auto 实现 flex 下的 align-self: flex-end",src:"https://codepen.io/Chokcoco/embed/PvOZMr?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/PvOZMr">
+  margin auto 实现 flex 下的 align-self: flex-end</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=i("",16),D=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"nav list by margin left auto ",src:"https://codepen.io/Chokcoco/embed/eaGmyv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/eaGmyv">
+  nav list by margin left auto </a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("h3",{id:"垂直方向上的多行居中",tabindex:"-1"},[t("垂直方向上的多行居中 "),s("a",{class:"header-anchor",href:"#垂直方向上的多行居中","aria-label":'Permalink to "垂直方向上的多行居中"'},"​")],-1),y=s("p",null,"OK，又或者，我们经常会有这样的需求，一大段复杂的布局中的某一块，高度或者宽度不固定，需要相对于它所在的剩余空间居中：",-1),A=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"自动margin快速垂直居中任意段落",src:"https://codepen.io/Chokcoco/embed/ZNvzdx?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/ZNvzdx">
+  自动margin快速垂直居中任意段落</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=i("",14),m=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"sticky footer by flex margin auto",src:"https://codepen.io/Chokcoco/embed/pmrbWX?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/pmrbWX">
+  sticky footer by flex margin auto</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),u=s("p",null,"上面的例子旨在介绍更多自动 margin 的使用场景。当然，这里不使用 flex 布局也是可以实现的，下面再给出一种不借助 flex 布局的实现方式：",-1),f=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"sticky footer by margin/paddig",src:"https://codepen.io/Chokcoco/embed/XwaKNJ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/Chokcoco/pen/XwaKNJ">
+  sticky footer by margin/paddig</a> by Chokcoco (<a href="https://codepen.io/Chokcoco">@Chokcoco</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),b=i("",3),E=[B,r,F,d,o,g,D,c,y,A,C,m,u,f,b];function _(v,x,q,w,S,P){return l(),n("div",null,E)}const j=a(k,[["render",_]]);export{V as __pageData,j as default};
