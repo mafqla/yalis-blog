3570ff267d434ee91ff3fa9c56363b80b5a42c77
mafqla <57307446+mafqla@users.noreply.github.com>
2024-04-22 03:27:46 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@87947c3a12842daa762b6ace1992ca1827e4a3a2 🚀

--- assets/knowledge_FrontEnd_css_1-基础_78-resize 实现强大的图片拖拽切换预览功能.md.9V-e1VFe.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as a,c as n,o as h,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const l="/assets/129582629-9919a138-8e70-44e2-818e-c70a06503fff.k0mTBu3C.gif",k="/assets/129585745-d004bbf8-5760-44ab-b4a2-65cbae64ab27.qAqXO7DV.gif",t="/assets/129587044-c6977b4d-0c8a-475a-a3df-91dd4ea63007.jjiwWsFq.gif",p="/assets/129590484-f7730867-3627-4209-b06d-7e394884fa92.N61ubAnF.gif",u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/78-resize 实现强大的图片拖拽切换预览功能.md","filePath":"knowledge/FrontEnd/css/1-基础/78-resize 实现强大的图片拖拽切换预览功能.md","lastUpdated":1713756294000}'),e={name:"knowledge/FrontEnd/css/1-基础/78-resize 实现强大的图片拖拽切换预览功能.md"},B=s("",12),F=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Switch Picture",src:"https://codepen.io/mafqla/embed/poYmePX?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/poYmePX">
-  Switch Picture</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),r=s("",33),d=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"PURE CSS SWITCH PIUTURE",src:"https://codepen.io/mafqla/embed/GReaWEd?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/mafqla/pen/GReaWEd">
-  PURE CSS SWITCH PIUTURE</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),D=[B,F,r,d];function g(y,A,C,o,c,m){return h(),n("div",null,D)}const f=a(e,[["render",g]]);export{u as __pageData,f as default};
