19b70be912c167c5f25c52990ea7d145cd226dde
mafqla <57307446+mafqla@users.noreply.github.com>
2024-05-06 02:04:51 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@7a0b5548d3e332798c259aa4bf976a035d8eae26 🚀

---
+++ assets/knowledge_FrontEnd_css_1-基础_35-你所不知道的 CSS 滤镜技巧与细节.md.ajXUwvR9.lean.js
@@ -0,0 +1,25 @@
+import{_ as n}from"./chunks/30364469-f3347b58-9896-11e7-82ec-ee7b32189a1a.IJIm08oG.js";import{_ as e,c as t,o as l,V as i,m as s,a}from"./chunks/framework.bW6FiXxS.js";const p="/assets/30325717-52d0fc70-97f8-11e7-8d08-dbb960c85118.Qn59LY9e.png",h="/assets/30367321-87ecd340-98a0-11e7-853e-ee1f557b9c85.9Rr2BYix.png",r="/assets/30368522-f746afba-98a3-11e7-93b8-92e2e2c1c622.soyOeMn7.png",z=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-基础/35-你所不知道的 CSS 滤镜技巧与细节.md","filePath":"knowledge/FrontEnd/css/1-基础/35-你所不知道的 CSS 滤镜技巧与细节.md","lastUpdated":1714960935000}'),k={name:"knowledge/FrontEnd/css/1-基础/35-你所不知道的 CSS 滤镜技巧与细节.md"},d=i("",4),o=s("iframe",{height:"600",style:{width:"100%"},scrolling:"no",title:"CSS3 filter (cancle filter by hover)",src:"https://codepen.io/mafqla/embed/QWoNYKZ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/QWoNYKZ">
+  CSS3 filter (cancle filter by hover)</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),B=i("",11),c=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS3 filter hover IMG",src:"https://codepen.io/mafqla/embed/OJqNdbv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/OJqNdbv">
+  CSS3 filter hover IMG</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),F=i("",10),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"使用 filter:blur 生成彩色阴影",src:"https://codepen.io/mafqla/embed/XWGdOpW?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWGdOpW">
+  使用 filter:blur 生成彩色阴影</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),D=i("",6),y=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"filter mix between blur and contrast",src:"https://codepen.io/mafqla/embed/oNVxmBY?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/oNVxmBY">
+  filter mix between blur and contrast</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=i("",19),A=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS fire| CSS filter mix",src:"https://codepen.io/mafqla/embed/RwdavpZ?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/RwdavpZ">
+  CSS fire| CSS filter mix</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),f=s("h2",{id:"文字融合动画",tabindex:"-1"},[a("文字融合动画 "),s("a",{class:"header-anchor",href:"#文字融合动画","aria-label":'Permalink to "文字融合动画"'},"​")],-1),_=s("p",null,[a("另外，我们可以在动画的过程中，动态改变元素滤镜的 "),s("code",null,"filter: blur()"),a(" 的值。")],-1),b=s("p",null,"利用这个方法，我们还可以设计一些文字融合的效果：",-1),m=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"word animation | word filter",src:"https://codepen.io/mafqla/embed/wvOGNJX?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/wvOGNJX">
+  word animation | word filter</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),u=i("",5),S=[d,o,B,c,F,g,D,y,C,A,f,_,b,m,u];function q(v,E,w,P,T,x){return l(),t("div",null,S)}const I=e(k,[["render",q]]);export{z as __pageData,I as default};
