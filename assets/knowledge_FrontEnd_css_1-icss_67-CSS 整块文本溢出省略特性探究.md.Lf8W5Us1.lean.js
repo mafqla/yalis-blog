401eed3fe68b2d7d42193a2d8de6e841a8ecc7e8
mafqla <57307446+mafqla@users.noreply.github.com>
2024-05-15 00:58:51 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@73be5336fcb6765ea2f049a7da443a604df9ba02 🚀

---
+++ assets/knowledge_FrontEnd_css_1-icss_67-CSS 整块文本溢出省略特性探究.md.Lf8W5Us1.lean.js
@@ -0,0 +1,13 @@
+import{_ as n,c as l,o as t,V as a,m as s,a as i}from"./chunks/framework.bW6FiXxS.js";const h="/assets/105368794-d9fd9580-5c3c-11eb-982e-ad9ba6f8b9d9.J6BlYh0l.png",e="/assets/110193172-59ea6280-7e6d-11eb-869e-4c5a4cb416ca.KpF0UHKT.png",p="/assets/110193395-8e125300-7e6e-11eb-8259-83baa387c947.b49K5IP7.png",k="/assets/110194312-d59ade00-7e72-11eb-9b31-223aa3de4371.eVKejzXh.png",q=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-icss/67-CSS 整块文本溢出省略特性探究.md","filePath":"knowledge/FrontEnd/css/1-icss/67-CSS 整块文本溢出省略特性探究.md","lastUpdated":1715734579000}'),B={name:"knowledge/FrontEnd/css/1-icss/67-CSS 整块文本溢出省略特性探究.md"},d=a("",6),F=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"inline-block 实现整块的溢出打点",src:"https://codepen.io/mafqla/embed/mdoLGNv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/mdoLGNv">
+  inline-block 实现整块的溢出打点</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),r=a("",11),D=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"整块超长溢出打点省略",src:"https://codepen.io/mafqla/embed/wvOjYww?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/wvOjYww">
+  整块超长溢出打点省略</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),o=a("",9),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"整块超长溢出打点省略",src:"https://codepen.io/mafqla/embed/NWJMOKP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/NWJMOKP">
+  整块超长溢出打点省略</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("p",null,[i("值得注意的是，在使用 "),s("code",null," -webkit-line-clamp"),i(" 的方案的时候，一定要配合 "),s("code",null,"white-space: normal"),i(" 允许换行，而不是不换行。这一点，非常重要。")],-1),y=s("p",null,"这样，我们就实现了全兼容的整块的超长打点省略了。",-1),A=s("p",null,[i("当然，"),s("code",null," -webkit-line-clamp"),i(" 本身也是存在一定的兼容性问题的，实际使用的时候还需要具体去取舍。")],-1),C=[d,F,r,D,o,g,c,y,A];function _(m,b,f,u,E,v){return t(),l("div",null,C)}const S=n(B,[["render",_]]);export{q as __pageData,S as default};
