b55e0ad11da04776b59506321b629a5578e9ee20
mafqla <57307446+mafqla@users.noreply.github.com>
2024-05-20 02:48:19 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@9da01fab80ec1ab7b2d4962b680cadd932719e68 🚀

---
+++ assets/knowledge_FrontEnd_css_1-icss_131-当路径动画遇到滚动驱动.md.YHDh0tEy.lean.js
@@ -0,0 +1,21 @@
+import{_ as a,a as n,b as h}from"./chunks/115992944-dcd4a180-a602-11eb-9ba7-6c346f4b2c50.ilXCpH72.js";import{_ as l,c as k,o as t,V as i,m as s}from"./chunks/framework.bW6FiXxS.js";const p="/assets/266765028-e551db4a-d06a-460d-968d-f5c6ba2e6300.j5w5vte3.gif",e="/assets/image-20240430093120860.pTfVyh4-.png",B="/assets/image-20240430093606871.2i9ElRXr.png",q=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-icss/131-当路径动画遇到滚动驱动.md","filePath":"knowledge/FrontEnd/css/1-icss/131-当路径动画遇到滚动驱动.md","lastUpdated":1716173138000}'),F={name:"knowledge/FrontEnd/css/1-icss/131-当路径动画遇到滚动驱动.md"},d=i("",21),r=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"使用 scroll-animation  实现滚动指示器进度条",src:"https://codepen.io/mafqla/embed/OJGGeJE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/OJGGeJE">
+  使用 scroll-animation  实现滚动指示器进度条</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),D=i("",23),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path Demo",src:"https://codepen.io/mafqla/embed/OJGGePw?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/OJGGePw">
+  CSS Motion Path Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=i("",11),C=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path Demo",src:"https://codepen.io/mafqla/embed/zYXXVGr?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/zYXXVGr">
+  CSS Motion Path Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),A=i("",30),o=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Scroll Driven & motion path &  SVG stroke-dasharray Demo",src:"https://codepen.io/mafqla/embed/ZEZZdbE?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/ZEZZdbE">
+  Scroll Driven &amp; motion path &amp;  SVG stroke-dasharray Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("p",null,"在灵活掌握了上述内容后，我们就可以利用路径动画及滚动驱动创造出各种妙趣横生的动画效果！",-1),m=s("p",null,"下面是我综合利用各种技巧，实现的一个纯 CSS 滚动动画效果，感受一下：",-1),E=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Scroll Driven",src:"https://codepen.io/mafqla/embed/zYXXVvr?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/zYXXVvr">
+  CSS Scroll Driven</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),f=[d,r,D,g,y,C,A,o,c,m,E];function u(v,b,_,M,T,S){return t(),k("div",null,f)}const I=l(F,[["render",u]]);export{q as __pageData,I as default};
---
+++ assets/knowledge_FrontEnd_css_1-icss_70-探秘神奇的运动路径动画 Motion Path.md.CZA0EaSC.lean.js
@@ -0,0 +1,29 @@
+import{_ as n,a as h,b as t}from"./chunks/115992944-dcd4a180-a602-11eb-9ba7-6c346f4b2c50.ilXCpH72.js";import{_ as k,c as l,o as p,V as i,m as s,a}from"./chunks/framework.bW6FiXxS.js";const e="/assets/115981572-55b60800-a5c7-11eb-8c50-fbd14588bdc5.SnA0gEJ1.gif",B="/assets/115981869-9151d180-a5c9-11eb-9ac3-47c91ad30df0.xfNqEc7P.gif",F="/assets/115993710-9c772280-a606-11eb-94e9-0b7068141ae8.rwHwGWIF.gif",d="/assets/115993869-49ea3600-a607-11eb-926e-62d34903d4bc.qQHdHEga.png",W=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-icss/70-探秘神奇的运动路径动画 Motion Path.md","filePath":"knowledge/FrontEnd/css/1-icss/70-探秘神奇的运动路径动画 Motion Path.md","lastUpdated":1716173138000}'),r={name:"knowledge/FrontEnd/css/1-icss/70-探秘神奇的运动路径动画 Motion Path.md"},D=i("",38),g=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path Demo",src:"https://codepen.io/mafqla/embed/rNRryBb?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/rNRryBb">
+  CSS Motion Path Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=i("",11),o=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path Demo",src:"https://codepen.io/mafqla/embed/VwRBpwj?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/VwRBpwj">
+  CSS Motion Path Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),C=i("",10),A=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path offset-anthor Demo",src:"https://codepen.io/mafqla/embed/BabPWam?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/BabPWam">
+  CSS Motion Path offset-anthor Demo</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("h2",{id:"运用-motion-path-制作动画效果",tabindex:"-1"},[a("运用 Motion Path 制作动画效果 "),s("a",{class:"header-anchor",href:"#运用-motion-path-制作动画效果","aria-label":'Permalink to "运用 Motion Path 制作动画效果"'},"​")],-1),m=s("p",null,"OK，上面我们基本把原理给过了一遍，下面我们就看看，运用 Motion Path，可以在实践中如何运用。",-1),f=s("h3",{id:"利用-motion-path-制作按钮效果",tabindex:"-1"},[a("利用 Motion Path 制作按钮效果 "),s("a",{class:"header-anchor",href:"#利用-motion-path-制作按钮效果","aria-label":'Permalink to "利用 Motion Path 制作按钮效果"'},"​")],-1),E=s("p",null,"利用运动路径，我们可以制作一些简单的按钮点击效果。在之前，我在 CodePen 上见到过这样一种按钮点击效果：",-1),u=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Bubbly button (Design by Gal Shir)",src:"https://codepen.io/mafqla/embed/poYZeox?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/poYZeox">
+  Bubbly button (Design by Gal Shir)</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),b=i("",6),_=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Button Animation with CSS Offset Paths",src:"https://codepen.io/mafqla/embed/XWGBMWw?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/XWGBMWw">
+  Button Animation with CSS Offset Paths</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),v=s("h3",{id:"利用-motion-path-绘制地图路径寻路动画",tabindex:"-1"},[a("利用 Motion-Path 绘制地图路径寻路动画 "),s("a",{class:"header-anchor",href:"#利用-motion-path-绘制地图路径寻路动画","aria-label":'Permalink to "利用 Motion-Path 绘制地图路径寻路动画"'},"​")],-1),S=s("p",null,"这个也是非常实用的，现在我们可以完全利用 CSS Motion-Path 实现地图上的寻路动画：",-1),P=s("p",null,"该 Demo 源自 Ahmad Emran：",-1),q=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Animation with offset-path | Only Using CSS & HTML",src:"https://codepen.io/ahmadbassamemran/embed/bXByBv?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/ahmadbassamemran/pen/bXByBv">
+  Animation with offset-path | Only Using CSS &amp; HTML</a> by Ahmad Emran (<a href="https://codepen.io/ahmadbassamemran">@ahmadbassamemran</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),w=s("h3",{id:"利用-motion-path-绘制路径动画",tabindex:"-1"},[a("利用 Motion-Path 绘制路径动画 "),s("a",{class:"header-anchor",href:"#利用-motion-path-绘制路径动画","aria-label":'Permalink to "利用 Motion-Path 绘制路径动画"'},"​")],-1),M=s("p",null,"又或者，我们利用 Path 能绘制任意路径的特性，实现各种我们想要的路径，譬如加入购物车的抛物线，或者各类运动轨迹，都不在话下，这里再提供一个 Demo：",-1),x=s("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Motion Path offset-path animation",src:"https://codepen.io/mafqla/embed/VwRBpYj?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/VwRBpYj">
+  CSS Motion Path offset-path animation</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),T=[D,g,y,o,C,A,c,m,f,E,u,b,_,v,S,P,q,w,M,x];function V(z,G,L,N,R,I){return p(),l("div",null,T)}const H=k(r,[["render",V]]);export{W as __pageData,H as default};
