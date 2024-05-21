3d173c231b8b44cecdafd47f331ab37b0ab17a20
mafqla <57307446+mafqla@users.noreply.github.com>
2024-05-21 03:18:46 +0000

    Deploying to gh-pages from @ mafqla/yalis-blog@ac73b000023c91db4db727a18f1357177c54adbe 🚀

---
+++ assets/knowledge_FrontEnd_css_1-icss_141-ChatGpt 多行文本打字效果.md.TUN-nzAm.lean.js
@@ -0,0 +1,13 @@
+import{_ as a,c as n,o as h,V as s,m as i}from"./chunks/framework.bW6FiXxS.js";const p="/assets/f8ec777674a445eeb99f5006fa21069c~tplv-k3u1fbpfcp-jj-m5.JnIRmmti.awebp",t="/assets/223f73b5f9924901a2f4b3af1505870b~tplv-k3u1fbpfcp-jj-mark.ULCogFu9.awebp",l="/assets/fd3cda3c47f54d3c95b535ffcafcaaa9~tplv-k3u1fbpfcp-jj-mark.GTk-eeHN.awebp",k="/assets/80b695c818dd4cb9835969da6d7f55d2~tplv-k3u1fbpfcp-jj-ma5.XNLb0Bxc.awebp",e="/assets/ed93caf4db1d4e1d998c9d180599dada~tplv-k3u1fbpfcp-jj-mar5.Nm0Y-hDo.awebp",B="/assets/f4f765e815f24db7a5694020a78b812d~tplv-k3u1fbpfcp-jj-ma75.h7yLq5-e.awebp",S=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"knowledge/FrontEnd/css/1-icss/141-ChatGpt 多行文本打字效果.md","filePath":"knowledge/FrontEnd/css/1-icss/141-ChatGpt 多行文本打字效果.md","lastUpdated":1716261378000}'),d={name:"knowledge/FrontEnd/css/1-icss/141-ChatGpt 多行文本打字效果.md"},F=s("",11),r=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"纯 CSS 实现文字输入效果",src:"https://codepen.io/mafqla/embed/MWdKRWp?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/MWdKRWp">
+  纯 CSS 实现文字输入效果</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),g=s("",30),D=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"background & display Animation DEMO",src:"https://codepen.io/mafqla/embed/oNRbONP?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/oNRbONP">
+  background &amp; display Animation DEMO</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),y=s("",15),o=i("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"多行打字效果",src:"https://codepen.io/mafqla/embed/PovZgoM?default-tab=html%2Cresult&editable=true&theme-id=light",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/mafqla/pen/PovZgoM">
+  多行打字效果</a> by mafqla (<a href="https://codepen.io/mafqla">@mafqla</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("",3),A=[F,r,g,D,y,o,c];function C(E,m,f,u,_,b){return h(),n("div",null,A)}const x=a(d,[["render",C]]);export{S as __pageData,x as default};
